51f4de180e4a2a6e091e5a9c3270fb6f5127835e
Shady Nawara <shady.nawara@outlook.com>
2022-03-17 03:27:07 -0500

    test device info

--- macUSPCIOTest/main.cpp
+++ macUSPCIOTest/main.cpp
@@ -11,7 +11,10 @@ io_connect_t macUSPCIO_driver_connection;
 int main(void)
 {
     InitMacUSPCIODriver();
-    printf("%i", ReadIoPortByte(0x40));
+    printf("Vendor Id: %x\n", ReadConfigPortWord(0x00));
+    printf("Device Id: %x\n", ReadConfigPortWord(0x02));
+    printf("Subsystem Vendor Id: %x\n", ReadConfigPortWord(0x2c));
+    printf("Subsystem Id: %x\n", ReadConfigPortWord(0x2e));
     CloseMacUSPCIODriver();
     return 0;
 }
